2757493b66203a303326325e0cff3269ff10d819
Xiaojie Hao <hxj.hao@gmail.com>
2021-01-25 22:35:52 -0800

    add fetch products to redux

--- frontend/src/containers/Sample/SampleContainer.js
+++ frontend/src/containers/Sample/SampleContainer.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sample from "../../components/Sample/Sample";
 import { Chip } from "@material-ui/core";
-import OrderInfo from "../Order/OrderInfo/OrderInfo";
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 
 function SampleContainer(props) {
+  const { onFetchProducts } = props;
+
+  useEffect(() => {
+    onFetchProducts();
+  }, []);
+
   return (
     <div>
       <Chip color="primary" label="This is a Sample Container" />
       <Sample clicked={props.clicked} />
-      <OrderInfo />
     </div>
   );
 }
 
-export default SampleContainer;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.products,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onFetchProducts: () => dispatch(actions.fetchProducts()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SampleContainer);
