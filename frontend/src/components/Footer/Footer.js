55f77bfe0b71886c016b1959639f6aa90eace4d1
Yanting Yin <yanting@Yantings-MacBook-Pro.local>
2021-01-26 22:15:22 -0500

    change footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,19 +1,45 @@
 import React from "react";
 import classes from "./Footer.module.css";
 import { useTheme } from "@material-ui/core/styles";
+import {Box, Button, Card, CardContent, Grid, TextField} from "@material-ui/core";
+
+
+import Bitmap from "../../assets/images/Bitmap.svg";
 
 function Footer(props) {
   const theme = useTheme();
   return (
-    <div
-      className={classes.container}
-      style={{ backgroundColor: theme.palette.primary.main }}
-    >
-      <div className={classes.content}>test</div>
-      <div className={classes.copyright}>
-        Copyright © 2021 The Love Team. - All rights reserved.
+      <div
+          className={classes.container}
+          style={{ backgroundColor: theme.palette.primary.main }}
+      >
+        <div
+            className={classes.container1}
+        >
+          <div className={classes.left}>
+          <p  className={classes.bigLetter}> Company</p>
+          <p className={classes.sLetter}>About us</p>
+          <p className={classes.sLetter}>Services</p>
+          <p className={classes.sLetter}>Projects</p>
+          <p className={classes.sLetter}>Career</p>
+          <p className={classes.sLetter}>Contacts</p>
+        </div>
+        <div className={classes.middle}>
+          <p className={classes.bigLetter}>Quick Links</p>
+          <p className={classes.sLetter}>Success Stories</p>
+          <p className={classes.sLetter}>Achievements</p>
+        </div>
+        <div className={classes.right}>
+          <p className={classes.bigLetter}>Follow Us</p>
+          <img src={Bitmap} className="image" alt="image" />
+        </div>
+          </div>
+        <div className={classes.copyright}>
+         <p> Copyright © 2021 The Love Team. - All rights reserved.</p>
+        </div>
       </div>
-    </div>
+
+
   );
 }
 
