a6c302340b36068a1ac01df390d6825201dc87df
Kate <starkatelyn247@gmail.com>
2021-01-28 19:35:35 -0800

    commiting regiter feature for review

---
+++ backend/src/main/java/com/theloveteam/web/dto/RegisterRequestBody.java
@@ -0,0 +1,79 @@
+package com.theloveteam.web.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class RegisterRequestBody {
+    @NotBlank(message = "Enter you first name")
+    private String firstName;
+
+    @NotBlank(message = "Enter you last name")
+    private String lastName;
+
+    @Email(message = "Enter a valid email address")
+    private String email;
+
+    @NotBlank(message = "Enter you password")
+    private String password;
+
+    @NotBlank(message = "Enter you phone number")
+    private String phone;
+
+    public RegisterRequestBody() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterRequestBody{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
--- backend/src/main/java/com/theloveteam/web/services/UserService.java
+++ backend/src/main/java/com/theloveteam/web/services/UserService.java
@@ -1,19 +1,51 @@
 package com.theloveteam.web.services;
 
 import com.theloveteam.web.dao.User;
+import com.theloveteam.web.dto.RegisterRequestBody;
 import com.theloveteam.web.model.LoginResult;
 import com.theloveteam.web.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    };
+
+    public List<User> findUserByEmail(String email) {return userRepository.findByEmail(email);}
+
+    // check if the email address already in the database
+    public boolean userExists(String email) {
+        return findUserByEmail(email).size() > 0;
+    }
+
+    // encrypt password and save the user info into the database
+    public User registerAccount(RegisterRequestBody registerRequestBody) {
+        // password encryption
+        String enPassword = passwordEncoder.encode(registerRequestBody.getPassword());
+        registerRequestBody.setPassword(enPassword);
+        // prepare the user object with the required registration fields and save to repository
+        User user = new User(registerRequestBody.getFirstName(),
+                registerRequestBody.getLastName(),
+                registerRequestBody.getEmail(),
+                registerRequestBody.getPassword(),
+                registerRequestBody.getPhone()
+        );
+        user.setEnabled(true);
+        return userRepository.save(user);
+    }
 
     public LoginResult validateEmailAndPassword(String email, String password) {
         List<User> users = userRepository.findByEmail(email);
@@ -26,4 +58,8 @@ public class UserService {
             return LoginResult.SYSTEM_ERROR;
         }
     }
+
+
+
+
 }
