4ae57a19f847cbc63db4aca93e388e5781daa148
WarrenSkywalker <hang.ren@wustl.edu>
2021-01-30 15:58:40 -0600

    revise app.dev.pro

--- frontend/src/containers/Sample/SampleContainer.js
+++ frontend/src/containers/Sample/SampleContainer.js
@@ -1,28 +1,15 @@
 import React from "react";
 import Sample from "../../components/Sample/Sample";
 import { Chip } from "@material-ui/core";
-
-import OrderInfo from "../Order/OrderInfo/OrderInfo";
-import Register from "../Register/Register";
-
 import * as actions from "../../store/actions/index";
 import { connect } from "react-redux";
 
-
 function SampleContainer(props) {
   return (
-    <div>
-
-      {/*<Chip color="primary" label="This is a Sample Container" />*/}
-      {/*<Sample clicked={props.clicked} />*/}
-      {/*<Register />*/}
-      {/*<OrderInfo />*/}
-
-
-      <Chip color="primary" label="This is a Sample Container" />
-      <Sample clicked={props.clicked} />
-
-    </div>
+      <div>
+        <Chip color="primary" label="This is a Sample Container" />
+        <Sample clicked={props.clicked} />
+      </div>
   );
 }
 
