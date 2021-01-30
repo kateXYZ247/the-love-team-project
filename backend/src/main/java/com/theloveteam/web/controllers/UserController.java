c39095418f520618a2acf6d8a886b07e1a44e860
Kate <starkatelyn247@gmail.com>
2021-01-29 16:27:08 -0800

    updated register feature returning Json object

--- backend/src/main/java/com/theloveteam/web/controllers/UserController.java
+++ backend/src/main/java/com/theloveteam/web/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.theloveteam.web.controllers;
 
+import com.theloveteam.web.constants.UrlConstants;
 import com.theloveteam.web.dto.RegisterRequestBody;
+import com.theloveteam.web.dto.RegisterResponseBody;
 import com.theloveteam.web.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -28,27 +33,52 @@ public class UserController {
 //        return "register";
 //    }
 
-    @PostMapping("/users/register")
-    public String register(@Valid @RequestBody RegisterRequestBody registerRequestBody, BindingResult bindingResult,
-                           RedirectAttributes redirect){
+    @PostMapping(UrlConstants.USERS_REGISTER)
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequestBody registerRequestBody, Errors errors){
         System.out.println(registerRequestBody.toString());
+        RegisterResponseBody responseBody = new RegisterResponseBody();
         // check if the user exists
         if (userService.userExists(registerRequestBody.getEmail())) {
-            bindingResult.addError(new FieldError("registerRequestBody",
-                    "email", "Email address already in use"));
+            responseBody.addErrorMsg("Email address already in use!");
+            return ResponseEntity.badRequest()
+                    .body(responseBody);
         }
-
-        if (bindingResult.hasErrors()) {
-            List<ObjectError> erorrs = bindingResult.getAllErrors();
-            for (int i = 0; i < erorrs.toArray().length; i++) {
-                System.out.println(erorrs.get(i).toString());
+        // check is there's any error from validation
+        if (errors.hasFieldErrors()) {
+            for (FieldError err : errors.getFieldErrors()) {
+                responseBody.addErrorMsg(err.getDefaultMessage());
             }
-            return "register";
+            return ResponseEntity.badRequest()
+                    .body(responseBody);
         }
-
-        //show success message
-        redirect.addFlashAttribute("message", "Congrats! You've successfully completed your registration");
+        // if there isn't any error, save data and give sucess message
         userService.registerAccount(registerRequestBody);
-        return "redirect:/users/login";
+        responseBody.setSuccessMessage("Congrats! You've successfully completed your registration.");
+        return ResponseEntity.ok(responseBody);
+
     }
+
+//    @PostMapping("/users/register")
+//    public String register(@Valid @RequestBody RegisterRequestBody registerRequestBody, BindingResult bindingResult,
+//                           RedirectAttributes redirect){
+//        System.out.println(registerRequestBody.toString());
+//        // check if the user exists
+//        if (userService.userExists(registerRequestBody.getEmail())) {
+//            bindingResult.addError(new FieldError("registerRequestBody",
+//                    "email", "Email address already in use"));
+//        }
+//
+//        if (bindingResult.hasErrors()) {
+//            List<ObjectError> erorrs = bindingResult.getAllErrors();
+//            for (int i = 0; i < erorrs.toArray().length; i++) {
+//                System.out.println(erorrs.get(i).toString());
+//            }
+//            return "register";
+//        }
+//
+//        //show success message
+//        redirect.addFlashAttribute("message", "Congrats! You've successfully completed your registration");
+//
+//        return "redirect:/users/login";
+//    }
 }
