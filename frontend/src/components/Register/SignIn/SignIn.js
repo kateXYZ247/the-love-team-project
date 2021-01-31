051ca0d4fe4a19beebc3f8309b6cf09b3b281f86
WarrenSkywalker <hang.ren@wustl.edu>
2021-01-30 20:38:51 -0600

    stamp

--- frontend/src/components/Register/SignIn/SignIn.js
+++ frontend/src/components/Register/SignIn/SignIn.js
@@ -3,7 +3,6 @@ import classes from "./SignIn.module.css";
 import Link from '@material-ui/core/Link';
 
 function SignIn(props) {
-    const preventDefault = (event) => event.preventDefault();
     return <div className={classes.member}> Already a member ? <Link href="/login" color="primary" >
         Sign in
     </Link></div>;
