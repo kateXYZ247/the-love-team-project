4f6f2fd68e2485cfce0aff119c6e4a9471ef2d54
Xiaojie Hao <hxj.hao@gmail.com>
2021-02-03 11:17:47 -0800

    adjust ProviderUpcomingServiceCard layout

--- frontend/src/components/ProviderUpcomingServiceCard/ProviderUpcomingServiceCard.js
+++ frontend/src/components/ProviderUpcomingServiceCard/ProviderUpcomingServiceCard.js
@@ -6,63 +6,65 @@ import UpcomingServiceItem from "./UpcomingServiceCardItem/UpcomingServiceItem";
 import Button from "@material-ui/core/Button";
 
 function ProviderUpcomingServiceCard(props) {
-  const { service, onContact, onAction, actionButtonText } = props;
+  const { service, onAction, actionButtonText } = props;
   return (
-    <Grid item xs={12} md={10}>
+    <Grid item xs={11} sm={8} lg={7}>
       <Box mt={2}>
         <Paper elevation={5}>
           <Box p={2}>
-            <Grid container spacing={2} justify="space-around">
-              <Grid item xs={11} md={7}>
-                <Box>
-                  <UpcomingServiceItem
-                    label="Service"
-                    value={service.productName}
-                    fontSize={"h6"}
-                    textColor="textPrimary"
-                  />
-                  <UpcomingServiceItem
-                    label="Date"
-                    value={service.startTime.toLocaleString()}
-                    fontSize={"h6"}
-                    textColor="textPrimary"
-                  />
-                  <UpcomingServiceItem
-                    label="Location"
-                    value={service.address}
-                    fontSize={"body1"}
-                    textColor="textPrimary"
-                  />
-                  <UpcomingServiceItem
-                    label="Note"
-                    value={service.note ? service.note : "None"}
-                    fontSize={"body1"}
-                    textColor="textPrimary"
-                  />
-                </Box>
+            <Grid container spacing={2}>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Service"
+                  value={service.productName}
+                  fontSize={"h6"}
+                  textColor="textPrimary"
+                />
               </Grid>
-              <Grid
-                container
-                item
-                xs={11}
-                md={5}
-                spacing={2}
-                alignContent="center"
-              >
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={onContact}
-                  >
-                    Contact Customer
-                  </Button>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button variant="outlined" color="primary" onClick={onAction}>
-                    {actionButtonText}
-                  </Button>
-                </Grid>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Date"
+                  value={service.startTime.toLocaleString()}
+                  fontSize={"h6"}
+                  textColor="textPrimary"
+                />
+              </Grid>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Location"
+                  value={service.address}
+                  fontSize={"body1"}
+                  textColor="textPrimary"
+                />
+              </Grid>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Direction"
+                  value={service.direction ? service.direction : "None"}
+                  fontSize={"body1"}
+                  textColor="textPrimary"
+                />
+              </Grid>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Pets"
+                  value={service.pets ? service.pets : "None"}
+                  fontSize={"body1"}
+                  textColor="textPrimary"
+                />
+              </Grid>
+              <Grid item xs={11} lg={6}>
+                <UpcomingServiceItem
+                  label="Note"
+                  value={service.note ? service.note : "None"}
+                  fontSize={"body1"}
+                  textColor="textPrimary"
+                />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <Button variant="outlined" color="primary" onClick={onAction}>
+                  {actionButtonText}
+                </Button>
               </Grid>
             </Grid>
           </Box>
