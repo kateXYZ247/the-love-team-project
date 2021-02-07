ef8a6d48d1225116a5b807cff29bbd614a410e1c
xuezhang <shirly_yukizhang@hotmail.com>
2021-02-06 16:47:47 -0800

    Add user cancel order feature

--- backend/src/main/java/com/theloveteam/web/constants/UrlConstants.java
+++ backend/src/main/java/com/theloveteam/web/constants/UrlConstants.java
@@ -12,5 +12,6 @@ public class UrlConstants {
 //    public static final String SERVICES_BY_PROVIDER_ID = "/services/";
     public static final String ORDERS = "/orders";
     public static final String ORDERS_BY_USER_ID = "/orders";
+    public static final String ORDERS_BY_ORDER_ID = "/orders/{orderId}";
     public static final String ORDERS_BY_ADMIN_ID = "/orders/admins/{adminId}";
 }
