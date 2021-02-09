d0708718c7fa3ae642b10a71c00a65fda31f3fac
Yanting Yin <yanting@Yantings-MacBook-Pro.local>
2021-02-09 10:08:06 -0500

    YYT_changeSectionTitle

--- frontend/src/components/HomePage/SectionTitle/SectionTitle.js
+++ frontend/src/components/HomePage/SectionTitle/SectionTitle.js
@@ -1,14 +1,37 @@
 import React from "react";
-import classes from "./SectionTitle.module.css";
+import { Grid, Box } from "@material-ui/core";
 
 function SectionTitle(props) {
   return (
-    <div className={classes.container}>
-      <div className={classes.title}>{props.title}</div>
-      {props.subtitle ? (
-        <div className={classes.subtitle}>{props.subtitle}</div>
-      ) : null}
-    </div>
+    <Grid container justify="center">
+      <Grid item xs={12}>
+        <Box
+          color="#2B292D"
+          fontFamily="Helvetica Neue, sans-serif"
+          fontSize={32}
+          lineHeight="56px"
+          text-align="center"
+          margin="10px auto"
+          fontWeight="fontWeightBold"
+          letterSpacing="-0.29px"
+        >
+          {props.title}
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Box
+          color="#2B292D"
+          fontFamily="Helvetica Neue, sans-serif"
+          fontSize={16}
+          letterSpacing="0"
+          lineHeight="34px"
+          text-align="center"
+          margin="10px auto"
+        >
+          {props.subtitle ? <div>{props.subtitle}</div> : null}
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
