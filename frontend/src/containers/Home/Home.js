3b45c1409b7ef8f2775d51be484985fe3da211d7
Yanting Yin <yanting@Yantings-MacBook-Pro.local>
2021-02-08 22:53:08 -0500

    YYT_changHomeTitle

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -3,12 +3,14 @@ import Book from "../../components/HomePage/Book/Book";
 import Sauce from "../../components/HomePage/Sauce/Sauce";
 import Process from "../../components/HomePage/Process/Process";
 import Products from "../../components/HomePage/Products/Products";
+import Title from "../../components/HomePage/Title/Title";
 import classes from "./Home.module.css";
 
 function Home(props) {
   // TODO: change to responsive design, optimize for mobile device
   return (
     <div className={classes.container}>
+      <Title />
       <Book />
       <Sauce />
       <Process />
