88532d3d758cb1765c14d4e5980dc067f2be77f0
Xiaojie Hao <hxj.hao@gmail.com>
2021-02-10 18:35:06 -0800

    show map with circle in pushed requests

--- frontend/src/components/PushedRequest/PushedRequest.js
+++ frontend/src/components/PushedRequest/PushedRequest.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import { Paper } from "@material-ui/core";
-import UpcomingServiceItem from "../ProviderUpcomingServiceCard/UpcomingServiceCardItem/UpcomingServiceItem";
-import {
-  LOCAL_DATETIME_OPTIONS,
-  LOCAL_TIME_OPTIONS,
-} from "../../constant/constant";
+import { Paper, Typography } from "@material-ui/core";
+import { LOCAL_SHORT_TIME_OPTIONS } from "../../constant/constant";
 import Button from "@material-ui/core/Button";
+import SmallGoogleMap from "../SmallGoogleMap/GoogleMap";
+import { MAP_RANDOM_OFFSET_AMPLITUDE } from "../../constant/service";
 
 function PushedRequest(props) {
   const { request, onAccept, onDecline } = props;
@@ -16,54 +14,73 @@ function PushedRequest(props) {
       <Box mt={2}>
         <Paper elevation={2}>
           <Box p={2}>
-            <Grid container spacing={0}>
-              <UpcomingServiceItem
-                fontSize="h6"
-                label="Service"
-                value={request.productName}
-              />
-              <UpcomingServiceItem
-                fontSize="h6"
-                label="Date"
-                value={
-                  request.startTime.toLocaleString([], LOCAL_DATETIME_OPTIONS) +
-                  " - " +
-                  request.endTime.toLocaleString([], LOCAL_TIME_OPTIONS)
-                }
-              />
-              <UpcomingServiceItem
-                labelLgWidth={1}
-                valueLgWidth={11}
-                label="Location"
-                value={request.address}
-              />
-              <UpcomingServiceItem
-                label="Pets"
-                value={request.pets ? request.pets : "None"}
-              />
-              <Grid item container spacing={2} justify="space-around">
-                <Grid item xs={5}>
-                  <Box mx={1} my={1} textAlign="center">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={onAccept}
-                    >
-                      Accept
-                    </Button>
-                  </Box>
-                </Grid>
-                <Grid item xs={5}>
-                  <Box mx={1} my={1} textAlign="center">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={onDecline}
-                    >
-                      Decline
-                    </Button>
-                  </Box>
-                </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={5} container justify="center">
+                <Box align="center">
+                  <Typography variant="h6">{request.productName}</Typography>
+                  <Typography variant="h6">
+                    {request.startTime.toDateString()}
+                  </Typography>
+                  <Typography variant="h6">
+                    {request.startTime.toLocaleString(
+                      [],
+                      LOCAL_SHORT_TIME_OPTIONS
+                    ) +
+                      " - " +
+                      request.endTime.toLocaleString(
+                        [],
+                        LOCAL_SHORT_TIME_OPTIONS
+                      )}
+                  </Typography>
+                  {request.pets && (
+                    <Typography variant="h6">{request.pets}</Typography>
+                  )}
+                  <Grid item container spacing={2} justify="space-around">
+                    <Grid item xs={6}>
+                      <Box mx={1} my={1} textAlign="center">
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={onAccept}
+                        >
+                          Accept
+                        </Button>
+                      </Box>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Box mx={1} my={1} textAlign="center">
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={onDecline}
+                        >
+                          Decline
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+
+              <Grid item xs={12} sm={7} container justify="center">
+                <SmallGoogleMap
+                  center={{
+                    lat:
+                      request.latitude +
+                      Math.random() * MAP_RANDOM_OFFSET_AMPLITUDE,
+                    lng:
+                      request.longitude +
+                      Math.random() * MAP_RANDOM_OFFSET_AMPLITUDE,
+                  }}
+                  circleCenter={{
+                    lat:
+                      request.latitude +
+                      Math.random() * MAP_RANDOM_OFFSET_AMPLITUDE,
+                    lng:
+                      request.longitude +
+                      Math.random() * MAP_RANDOM_OFFSET_AMPLITUDE,
+                  }}
+                />
               </Grid>
             </Grid>
           </Box>
