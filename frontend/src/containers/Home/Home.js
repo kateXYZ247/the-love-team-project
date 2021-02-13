8a6cff8e81ac7823c4fd1a88f73e39b21563d640
Xiaojie Hao <hxj.hao@gmail.com>
2021-02-13 10:49:34 -0800

    Home-book

--- frontend/src/containers/Home/Home.js
+++ frontend/src/containers/Home/Home.js
@@ -3,17 +3,17 @@ import Book from "../../components/HomePage/Book/Book";
 import Sauce from "../../components/HomePage/Sauce/Sauce";
 import Process from "../../components/HomePage/Process/Process";
 import Products from "../../components/HomePage/Products/Products";
-import classes from "./Home.module.css";
+import Box from "@material-ui/core/Box";
 
 function Home(props) {
   // TODO: change to responsive design, optimize for mobile device
   return (
-    <div className={classes.container}>
+    <Box p={5}>
       <Book />
       <Sauce />
       <Process />
       <Products />
-    </div>
+    </Box>
   );
 }
 
--- frontend/src/shared/axios_instance.js
+++ frontend/src/shared/axios_instance.js
@@ -1,23 +1,17 @@
 import axios from "axios";
-import {LOCAL_STORAGE_TOKEN_KEY} from "../constant/auth";
-import jwtDecode from 'jwt-decode';
-import store from '../index';
-import { AUTH_LOGOUT } from '../store/actions/actionTypes';
-import {logoutAndMessage} from '../store/actions'
-import * as actions from '../store/actions'
-import {logout} from '../store/actions/auth'
-import {setMessage} from '../store/actions/message'
-import {MESSAGE_TYPE} from '../constant/message'
-
+import { LOCAL_STORAGE_TOKEN_KEY } from "../constant/auth";
+import jwtDecode from "jwt-decode";
+import store from "../index";
+import { logout } from "../store/actions/auth";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 // Set the AUTH token for any request
 instance.interceptors.request.use(function (config) {
   const token = sessionStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
-  const decodeToken = token ? jwtDecode(token) : "";
+  //const decodeToken = token ? jwtDecode(token) : "";
   //console.log("decoded token is", decodeToken);
   //console.log("is_user: " ,decodeToken.sub.toString().includes(':"user"'));
   //console.log("is_provider: " ,decodeToken.sub.toString().includes(':"provider"'));
@@ -26,33 +20,35 @@ instance.interceptors.request.use(function (config) {
 });
 
 instance.interceptors.response.use(
-    (response) => new Promise((resolve, reject) => {
+  (response) =>
+    new Promise((resolve, reject) => {
       resolve(response);
-    }), (error) => {
-      if (!error.response) {
-        return new Promise((resolve, reject) => {
-          reject(error);
-        });
-      }
-      if(error.response.status === 403) {
-        const token = sessionStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
-        const decodeToken = token ? jwtDecode(token) : "";
-        if (decodeToken.sub.toString().includes(':"provider"')) {
-          localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
-          sessionStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
-          store.dispatch(logout());
-          //window.location ="/provider/login";
-        }else if (decodeToken.sub.includes(':"user"')) {
-          localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
-          sessionStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
-          store.dispatch(logout());
-        } //else admin
-      } else {
-        return new Promise((resolve, reject) => {
-          reject(error);
-        });
-      }
+    }),
+  (error) => {
+    if (!error.response) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
     }
+    if (error.response.status === 403) {
+      const token = sessionStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
+      const decodeToken = token ? jwtDecode(token) : "";
+      if (decodeToken.sub.toString().includes(':"provider"')) {
+        localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+        sessionStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+        store.dispatch(logout());
+        //window.location ="/provider/login";
+      } else if (decodeToken.sub.includes(':"user"')) {
+        localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+        sessionStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+        store.dispatch(logout());
+      } //else admin
+    } else {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+  }
 );
 
 export default instance;
