2b23f399ca7e32997bbb2a7efb48d8110c49d7ac
Xiaojie Hao <hxj.hao@gmail.com>
2021-02-13 17:40:45 -0800

    clear warnings ...

--- frontend/src/components/Order/AddressCard/AddressCard.js
+++ frontend/src/components/Order/AddressCard/AddressCard.js
@@ -10,10 +10,7 @@ import {
 import CardTitle from "../../UI/CardTitle/CardTitle";
 import { addressTypes } from "../../../constant/order";
 
-import OrderInfo from "../../../containers/Order/OrderInfo/OrderInfo";
-
 import AddressInput from "../AddressInput/AddressInput";
-
 
 function AddressCard(props) {
   const {
